255ee1dd68459ae3b648833b256de0f119c782e9
renn27 <mrendialamsyah12@gmail.com>
2024-03-02 08:45:25 +0700

    Finishing feature circle formula

--- src/Main.kt
+++ src/Main.kt
@@ -1,3 +1,15 @@
+import kotlin.math.PI
+
+class Circle(private val radius: Double) {
+    fun calculateArea(): Double {
+        return PI * radius * radius
+    }
+
+    fun calculatePerimeter(): Double {
+        return 2 * PI * radius
+    }
+}
+
 class Triangle() {
     fun calculate() {
         val height = 5.0
@@ -9,7 +21,9 @@ class Triangle() {
 }
 
 fun main() {
+    val radius = 5.0
+    val circle = Circle(radius)
     Triangle().calculate()
+    println("Area of the circle: ${circle.calculateArea()}")
+    println("Perimeter of the circle: ${circle.calculatePerimeter()}")
 }
-
-
